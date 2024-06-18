118f4a300619ab2d7a4b5121517105939a27ba29
Arne <arne.goolaerts@resengo.com>
2024-06-18 17:20:16 +0200

    wip: add blog list

--- src/app/pages/feedback/feedback.component.ts
+++ src/app/pages/feedback/feedback.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RatingSelectComponent } from '../../components/rating-select/rating-select.component';
 import { SellRentComponent } from '../../components/sell-rent/sell-rent.component';
+import { BlogListComponent } from '../../components/blog-list/blog-list.component';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss',
-  imports: [RatingSelectComponent, SellRentComponent],
+  imports: [RatingSelectComponent, SellRentComponent, BlogListComponent],
 })
 export class FeedbackComponent {}
