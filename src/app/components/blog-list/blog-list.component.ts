6b7b9e3de677cf87d0459fdd5a182fb8179c94d5
Arne <arne.goolaerts@resengo.com>
2024-06-18 18:16:19 +0200

    Create blog list and card component

--- src/app/components/blog-list/blog-list.component.ts
+++ src/app/components/blog-list/blog-list.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { BlogCardComponent } from '../blog-card/blog-card.component';
 
 @Component({
   selector: 'app-blog-list',
   standalone: true,
-  imports: [],
   templateUrl: './blog-list.component.html',
-  styleUrl: './blog-list.component.scss'
+  styleUrl: './blog-list.component.scss',
+  imports: [BlogCardComponent],
 })
-export class BlogListComponent {
-
-}
+export class BlogListComponent {}
